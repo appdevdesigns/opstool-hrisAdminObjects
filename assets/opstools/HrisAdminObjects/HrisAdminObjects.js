6261838b7593e6bed124e752c2c2c343248d5646
Johnny Hausman <johnny@appdevdesigns.net>
2015-09-29 14:58:38 +0700

    +ADD: added loading progress updates.

--- assets/opstools/HrisAdminObjects/HrisAdminObjects.js
+++ assets/opstools/HrisAdminObjects/HrisAdminObjects.js
@@ -1,8 +1,14 @@
 steal(
         // List your Page's dependencies here:
-        // 'appdev',
+        'appdev',
+        function() {
+            AD.ui.loading.resources(9);
+        },
         '//opstools/HrisAdminObjects/hrisAdminObjects.css'
 ).then(
+        function() {
+            AD.ui.loading.completed(1);
+        },
         "opstools/HrisAdminObjects/controllers/ObjectList.js",
         "opstools/HrisAdminObjects/controllers/SetList.js",
         "opstools/HrisAdminObjects/controllers/AttrList.js",
@@ -11,7 +17,10 @@ steal(
         "opstools/HrisAdminObjects/controllers/FormSetNew.js",
         "site/labels/HrisAdminObjects.js"
 ).then(
+        function() {
+            AD.ui.loading.completed(7);
+        },
         "opstools/HrisAdminObjects/controllers/HrisAdminObjects.js"
 ).then(function(){
-
+        AD.ui.loading.completed(1);
 });
