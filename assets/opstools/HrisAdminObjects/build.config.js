280a2ae0ebf84aa574f1d5845427a73884b3a195
Pong <wongpratan@gmail.com>
2016-02-08 11:25:04 +0700

    Edit build config file

--- assets/opstools/HrisAdminObjects/build.config.js
+++ assets/opstools/HrisAdminObjects/build.config.js
@@ -1,20 +1,18 @@
 module.exports={
-    // map: {
-    // },
-    // paths: {
-    // },
-    shim : {
-
-        //// Don't include our labels in our production.js
-        'site/labels/HrisAdminObjects.js' : { packaged:false, ignore:true }
+    "map": {
+        "jquery/jquery" : "jquery",
+        "GenericList": "GenericList.js",
+    },
+    "paths": {
+        "jquery" : "js/jquery.min.js",                  // 'http://code.jquery.com/jquery-1.11.1.min.js'
+        "GenericList.js": "js/GenericList.js",
+    },
+    "ext": {
+        "ejs": "can/view/ejs/system"
+    },
+    "buildConfig": {
+        "map": {
+            "can/util/util": "can/util/domless/domless"
+        }
     }
-    // ext: {
-    //     js: "js",
-    //     css: "css",
-    //     less: "steal/less/less.js",
-    //     coffee: "steal/coffee/coffee.js",
-    // }
 };
-    
-
-
